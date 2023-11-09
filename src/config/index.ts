4bd81341a5fb00ac5fa0b5f0e06bb6c38c61fa34
js-0127 <w20021125@qq.com>
2023-11-09 18:46:32 +0800

    feat:测试5

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,4 +2,4 @@ import minioConfig from "./minio.config";
 import redisConfig from "./redis.config";
 import mailConfig from "./mail.config";
 import tokenConfig from "./token.config";
-export default [redisConfig, minioConfig,mailConfig,tokenConfig];
+export default [redisConfig, minioConfig,mailConfig,tokenConfig]
